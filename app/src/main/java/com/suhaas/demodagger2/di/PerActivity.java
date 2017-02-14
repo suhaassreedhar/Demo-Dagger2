711e35684405fddd46346887851b07133251da0e
Apple <suhaas.sreedhar@gmail.com>
2017-02-14 13:19:21 +0530

    Added custom annontations of dagger2

---
+++ app/src/main/java/com/suhaas/demodagger2/di/PerActivity.java
@@ -0,0 +1,12 @@
+package com.suhaas.demodagger2.di;
+
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PerActivity {
+}
